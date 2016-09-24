1839ff89c122c5db30fdf99b7849d0eaf7357d0e
Ken <0909758@hr.nl>
2016-09-24 16:17:23 +0200

    Added project files

---
+++ app/models/user.rb
@@ -0,0 +1,18 @@
+class User < ActiveRecord::Base
+  has_secure_password
+  has_many :posts, dependent: :destroy, foreign_key: 'user_id'
+
+  validates :first_name,
+            presence: true
+  validates :last_name,
+            presence: true
+  validates :email,
+            presence: true,
+            uniqueness: true,
+            format: {
+                with: /\A([^@\s]+)@((?:[-a-z0-9]+\.)+[a-z]{2,})\z/i
+            }
+  def to_s
+    "#{first_name} #{last_name}"
+  end
+end
